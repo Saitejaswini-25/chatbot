808aacfdeae390bdd716b1696596a44c5104bb23
Gorantla Sai Tejaswini <145936357+Saitejaswini-25@users.noreply.github.com>
2025-04-18 12:47:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '', // empty string for Vercel
+  base: '/', // empty string for Vercel
   plugins: [react()],
 });
